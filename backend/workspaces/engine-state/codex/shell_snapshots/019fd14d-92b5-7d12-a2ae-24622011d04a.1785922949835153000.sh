bec1b7a97e0fcd56a72f98e2ad1c9d07e9817091
BudgetLoop <budgetloop@local>
2026-08-05 17:42:49 +0800

    init

---
+++ backend/workspaces/engine-state/codex/shell_snapshots/019fd14d-92b5-7d12-a2ae-24622011d04a.1785922949835153000.sh
@@ -0,0 +1,29 @@
+# Snapshot file
+# Unset all aliases to avoid conflicts with functions
+unalias -a 2>/dev/null || true
+# Functions
+
+# setopts 2
+setopt nohashdirs
+setopt login
+
+# aliases 2
+alias run-help=man
+alias which-command=whence
+
+# exports 15
+export BUDGETLOOP_AI_MANAGED=1
+export CODEX_HOME=/Users/qi/Desktop/BudgetLoop/backend/workspaces/engine-state/codex
+export HOME=/Users/qi/Desktop/BudgetLoop/backend/workspaces/engine-state/codex
+export HTTPS_PROXY=http://127.0.0.1:7897
+export HTTP_PROXY=http://127.0.0.1:7897
+export LANG=zh_CN.UTF-8
+export LOGNAME=qi
+export NO_PROXY=localhost,127.0.0.1,::1,.local,.lan,seeed.local,web-ai-media-editor.cn,aigateway.sangfor.com,10.6.192.170
+export OPENAI_API_KEY=blrt1.eyJhdWQiOiJtYW5hZ2VkLWFpLWFwcCIsImV4cCI6MTc4NTk2NjE0OSwibW9kZWwiOiJkZWVwc2Vlay12NC1wcm8tMjAyNjA2IiwicnVuX2lkIjoiODBhMmU2OWUtMGNjNi00OWEyLWFhZDAtZDZlOWE5YTBkMTY3IiwidiI6MX0.tjd5U9sjE476kPbmu_rr1QUw-x5ZCVzsghK60XGc5bM
+export OPENAI_BASE_URL=http://127.0.0.1:8000/api/runtime/ai/v1
+export OPENAI_MODEL=deepseek-v4-pro-202606
+export -T PATH path=( /usr/local/bin /System/Cryptexes/App/usr/bin /usr/bin /bin /usr/sbin /sbin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin /var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin /pkg/env/global/bin /Users/qi/Desktop/BudgetLoop/backend/workspaces/engine-state/codex/tmp/arg0/codex-arg0UhVBS2 /Applications/ChatGPT.app/Contents/Resources /Users/qi/.local/bin /Users/qi/.kimi-code/bin /opt/homebrew/bin /opt/homebrew/sbin /Applications/iTerm.app/Contents/Resources/utilities )
+export SHELL=/bin/bash
+export TMPDIR=/var/folders/z6/n6_xdt5d3971sq2tx3bthx1w0000gn/T/
+export __CF_USER_TEXT_ENCODING=0x1F5:0x19:0x34
